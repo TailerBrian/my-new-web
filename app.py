b51e39c26727a5a05718f858d5d98295a6ef02f6
Tailer <195159150+TailerBrian@users.noreply.github.com>
2026-05-13 08:51:00 -0300

    adicionada a exibicao do dataframe conforme o curso

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import scipy.stats
 import streamlit as st
 import time
 
-# estas são variáveis persistentes preservadas à medida que o Streamlit executa novamente esse script
+# estas são variáveis persistentes preservadas à medida que o Streamlin executa novamente esse script
 if 'experiment_no' not in st.session_state:
     st.session_state['experiment_no'] = 0
 
@@ -35,16 +35,18 @@ number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
 start_button = st.button('Executar')
 
 if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
     st.session_state['experiment_no'] += 1
-    st.write(f'Executando o experimento nº {st.session_state["experiment_no"]} de {number_of_trials} tentativas.')
-    
     mean = toss_coin(number_of_trials)
-    
-    # Guarda os resultados do teste atual na tabela de histórico
     st.session_state['df_experiment_results'] = pd.concat([
         st.session_state['df_experiment_results'],
-        pd.DataFrame([{'no': st.session_state['experiment_no'], 'iterations': number_of_trials, 'mean': mean}])
-    ], ignore_index=True)
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
 
-# Exibe a tabela com o histórico de todos os testes feitos abaixo do gráfico
-st.dataframe(st.session_state['df_experiment_results'])
+st.write(st.session_state['df_experiment_results'])
